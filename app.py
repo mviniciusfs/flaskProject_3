df4ce2d778ffa4f840769bfada27a0b72886a474
mviniciusfs <50427044+mviniciusfs@users.noreply.github.com>
2021-11-19 19:46:55 -0300

    start

---
+++ app.py
@@ -0,0 +1,17 @@
+import random
+
+import requests
+from flask import Flask, render_template, request
+import json
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    app.run()
